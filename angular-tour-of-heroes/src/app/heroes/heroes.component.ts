e18c9de1082c7fb6d53220fc567e5f2880545189
Sean Bannon <seannbannon@gmail.com>
2022-06-14 10:51:41 -0500

    importing hero component from hero interface

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
+  // components CSS element selector
   selector: 'app-heroes',
+  // location of components template file
   templateUrl: './heroes.component.html',
+  // location of components private CSS styles
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
 
+// export allows component class to be imported somewhere else
+export class HeroesComponent implements OnInit {
+  hero = 'Windstorm';
   constructor() { }
 
+  // ngOnInit is a lifecycle hook-- good place for initialization logic
   ngOnInit(): void {
   }
 
